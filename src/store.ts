6b682e454df436d6149b075b294d325db1ef0ac7
devilune <devilune@wizlog.io>
2018-03-01 01:10:44 +0900

    Redux DevTools

--- src/store.ts
+++ src/store.ts
@@ -4,18 +4,21 @@ import { routerMiddleware, routerReducer } from 'react-router-redux';
 import logger from 'redux-logger';
 
 import hello, { HelloState } from './redux/modules/hello';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 export const appHistory = createHistory();
 const router = routerMiddleware(appHistory);
 
 export default createStore(
-    combineReducers({
-        hello,
-        router: routerReducer
-    }),
+  combineReducers({
+    hello,
+    router: routerReducer
+  }),
+  composeWithDevTools(
     applyMiddleware(router, logger)
+  )
 );
 
 export type AppState = {
-    hello: HelloState
+  hello: HelloState
 };
