eb2064683d4faa6110d08891f2e3f0dc86f0bcfe
devilune <devilune@wizlog.io>
2018-03-12 22:40:15 +0900

    Fixed layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,59 +1,88 @@
 import * as React from 'react';
-import { Container, Image, List, Menu, Segment } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import Menu from 'semantic-ui-react/dist/commonjs/collections/Menu/Menu';
+import Container from 'semantic-ui-react/dist/commonjs/elements/Container/Container';
+import Dropdown from 'semantic-ui-react/dist/commonjs/modules/Dropdown/Dropdown';
+// import Image from 'semantic-ui-react/dist/commonjs/elements/Image/Image';
+import Sidebar from 'semantic-ui-react/dist/commonjs/modules/Sidebar/Sidebar';
 
-const logo = require('../logo.svg');
+// const logo = require('../logo.svg');
+
 const Layout: React.SFC = ({children}) => (
-  <div
-    style={{
-      display: 'flex',
-      minHeight: '100vh',
-      flexDirection: 'column',
-    }}
-  >
-    <Menu fixed="top" inverted>
-      <Container>
-        <Menu.Item as={Link} to={'/'} header>
-          <Image
-            size="mini"
-            src={logo}
-            style={{marginRight: '1.5em'}}
-          />
-          ANQ
-        </Menu.Item>
-        <Menu.Item as={Link} to={'/users'}>
-          User
-        </Menu.Item>
-        <Menu.Item as={Link} to={'/hello'}>
-          Hello
-        </Menu.Item>
-      </Container>
-    </Menu>
+  <>
+    <Sidebar
+      as={Menu}
+      visible
+      inverted
+      fixed={'left'}
+      vertical
+    >
+      <Menu.Item as={Link} to={'/'} header>
+        {/*<Image*/}
+          {/*avatar*/}
+          {/*size="mini"*/}
+          {/*src={logo}*/}
+        {/*/>*/}
+        <span style={{verticalAlign: 'middle'}}>ANQ</span>
+      </Menu.Item>
+      <Menu.Item as={Link} to={'/users'}>
+        User
+      </Menu.Item>
+      <Menu.Item as={Link} to={'/hello'}>
+        Hello
+      </Menu.Item>
+    </Sidebar>
+    {/*<div className="ui visible inverted left vertical sidebar menu">*/}
+      {/*<a className="item">Home </a>*/}
+      {/*<a className="item">Page 1</a>*/}
+      {/*<a className="item">Page 2</a>*/}
+    {/*</div>*/}
 
-    <Container text style={{marginTop: '7em', flex: '1'}}>
+    <Menu
+      fixed={'top'}
+    >
+      <Menu.Item>
+        HOME
+      </Menu.Item>
+      <Menu.Item>
+        HOME
+      </Menu.Item>
+      <Menu.Menu position={'right'}>
+        <Dropdown text="Dropdown" pointing className="link item">
+          <Dropdown.Menu>
+            <Dropdown.Item>List Item</Dropdown.Item>
+            <Dropdown.Item>List Item</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Header>Header Item</Dropdown.Header>
+            <Dropdown.Item>
+              <i className="dropdown icon" />
+              <span className="text">Submenu</span>
+              <Dropdown.Menu>
+                <Dropdown.Item>List Item</Dropdown.Item>
+                <Dropdown.Item>List Item</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown.Item>
+            <Dropdown.Item>List Item</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </Menu.Menu>
+    </Menu>
+    {/*<div className="ui top fixed menu">*/}
+      {/*<a className="item menu-trigger">Menu</a>*/}
+      {/*<a className="item">Page 1</a>*/}
+      {/*<a className="item">Page 2</a>*/}
+    {/*</div>*/}
+
+    <Container text style={{marginTop: '3em'}}>
       {children}
     </Container>
 
-    <Segment
-      inverted
-      vertical
-      style={{margin: '5em 0em 0em'}}
-    >
-      <Container textAlign="center">
-        <Image
-          centered
-          size="mini"
-          src={logo}
-        />
-        <List horizontal inverted divided link>
-          <List.Item as="a" href="#">Site Map</List.Item>
-          <List.Item as="a" href="#">Contact Us</List.Item>
-          <List.Item as="a" href="#">Terms and Conditions</List.Item>
-          <List.Item as="a" href="#">Privacy Policy</List.Item>
-        </List>
-      </Container>
-    </Segment>
-  </div>
+    {/*<div className="pusher">*/}
+      {/*<div className="ui basic segment">*/}
+        {/*<h3 className="ui header">Hello there</h3>*/}
+      {/*</div>*/}
+    {/*</div>*/}
+  </>
 );
 
 export default Layout;
